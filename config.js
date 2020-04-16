b9a34dbe522672e8c3a4fc030ceeb2c457183a66
Glitch (openclassroom) <none>
2020-04-16 01:14:30 +0000

    Ready to begin adding in socketio

--- config.js
+++ config.js
@@ -2,5 +2,7 @@ module.exports = {
   "NAME":"Open Classroom Demo",
   "database":"sqlite://.data/storage.sqlite",
   "require_verified_email":true, // Require a verified google email
-  "trusted_domains":[]// Trusted email domains
+  "trust_all_domains": false, 
+  "trusted_domains":["gmail.com","googlemail.com"],// Trusted email domains
+  "teachers":["awesomecatstudio@gmail.com"] // Emails of teachers you trust
 }
--- server.js
+++ server.js
@@ -17,10 +17,19 @@ app.set("views", path.join(__dirname, "/public"));
 // Init Database
 const Endb = require("endb");
 const userdb = new Endb({ namespace: "users", uri: config.database });
+const miscdb = new Endb({ namespace: "misc", uri: config.database });
+async function initDB() {
+  if (!(await miscdb.has("teachers"))) {
+    await miscdb.set("teachers", []);
+  }
+}
+initDB()
+  .then(console.log)
+  .catch(console.error);
 // Extras
 // app.enable('view cache');
 // our nonexistent default array of dreams
-// I killed the template yes
+// I destoryed the template yes
 
 // make all the files in 'public' available
 // https://expressjs.com/en/starter/static-files.html
@@ -44,6 +53,11 @@ app.use(
 var bodyParser = require("body-parser");
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+// Setup sessions for socketio
+var ios = require('socket.io-express-session');
+io.use(ios(session));
 // Setup Auth
 var googleAuth = auth.create_gsignin(process.env.GSIGNIN_CLIENT_ID);
 // Response handlers
@@ -51,11 +65,17 @@ async function handler(req, res) {
   var templaterData = {};
   templaterData["brand"] = config["NAME"];
   templaterData["GSIGNIN_CLIENT_ID"] = process.env.GSIGNIN_CLIENT_ID;
+  let isTeacher = false;
+  var userdata = {}
   if (req.session.sessionid) {
+    userdata = await userdb.get(req.session.sessionid);
     templaterData["loginstatus"] = "loggedin";
+    isTeacher = (await miscdb.get("teachers")).includes(userdata["email"]) || (config.teachers.includes(userdata["email"]))
   } else {
     templaterData["loginstatus"] = "loggedout";
   }
+  
+  templaterData["teacher"] = isTeacher;
   if (req.originalUrl == "/") {
     res.render("index.html", templaterData);
   } else if (req.originalUrl == "/classrooms") {
@@ -77,7 +97,7 @@ async function handler(req, res) {
         id,
         process.env.GSIGNIN_CLIENT_ID + ".apps.googleusercontent.com"
       )
-        .then(function(payload) {
+        .then(async function(payload) {
           //res.send(JSON.stringify(payload));
           // Security
           let email_domain = payload["email"].substring(
@@ -85,10 +105,15 @@ async function handler(req, res) {
           );
           if (!payload["email_verified"] && config.require_verified_email) {
             res.send("You need a verified email for your google account");
-          } else if (!config.trusted_domains.includes(email_domain)) {
+          } else if (!config.trusted_domains.includes(email_domain) && !config.trust_all_domains) {
             res.send("Sorry your domain " + email_domain + " is not allowed");
           } else {
+            let id = payload["sub"];
             req.session.sessionid = payload["sub"];
+            if (!(await userdb.has(payload["sub"]))) {
+              //userdb.set(payload["sub"], payload);
+            }
+            userdb.set(payload["sub"], payload);
             res.send("Ok!");
           }
         })
@@ -102,6 +127,20 @@ async function handler(req, res) {
     } else {
       res.send("No?");
     }
+  } else if (req.originalUrl == "/app") {
+    res.render("app.html", templaterData);
+  } else if (req.originalUrl == "/api/user_data") {
+    // let userdata = await userdb.get(req.session.sessionid);
+    //console.log(userdata);
+    res.send(JSON.stringify(userdata));
+  } else if (req.originalUrl == "/api/client_data") {
+    // let userdata = await userdb.get(req.session.sessionid);
+    //console.log(userdata);
+    let stripped_data = {
+      full_name: userdata["name"],
+      profile_pic: userdata["picture"]
+    };
+    res.send(JSON.stringify(stripped_data));
   }
 }
 
@@ -110,6 +149,9 @@ app.get("/classrooms", handler);
 app.get("/signin", handler);
 app.post("/auth", handler);
 app.post("/signout", handler);
+app.get("/app", handler);
+app.get("/api/user_data", handler);
+app.get("/api/client_data", handler);
 app.use(express.static("public"));
 // Don't send the default array of dreams to the webpage
 // Also removed from template
